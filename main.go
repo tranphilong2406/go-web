f3e18c9217b1b9dc3416543e1ad0b0c97afb79a0
tranphilong2406 <caybut65@gmail.com>
2023-01-11 15:14:59 +0700

    update

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"go-web-server/api"
-	"go-web-server/auth"
+	"go-web-server/api/auth"
+	"go-web-server/api/todo"
 	"go-web-server/middlewares"
 	"net/http"
 )
@@ -18,5 +19,9 @@ func main() {
 	e.POST("/update-user", middlewares.TokenHandle(auth.SignIn))
 
 	e.GET("/get-user/:id", middlewares.TokenHandle(api.GetUser))
+	e.POST("/add-todo", middlewares.TokenHandle(todo.AddTodo))
+	e.POST("/edit-todo", middlewares.TokenHandle(todo.EditTodo))
+	e.GET("/get-list-todo", middlewares.TokenHandle(todo.GetListTodo))
+	e.GET("/get-todo/:id", todo.GetTodo)
 	e.Logger.Fatal(e.Start(":8080"))
 }
